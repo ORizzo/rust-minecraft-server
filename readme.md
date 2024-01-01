1e1704f2ea8fa1f6296afa8d73bdd821119148dd
ORizzo <henriquerizzomor@gmail.com>
2024-01-01 13:29:06 -0300

    feat: :sparkles: Add setup shell

--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-# REQUISITOS
+## SETUP(manual)
 
-1. cd ../
+1. sudo apt update
+2. sudo apt upgrade
+3. cd ../
+4. sudo apt-get install wine
+5. wget ${no link da última release}
+6. wine ${nome do executável}
 
-2. sudo apt-get install wine
+## SETUP(automático)
 
-3. wget ${no link da última release}
-
-4. wine ${nome do executável}
+1. baixar o setup.sh
+2. sudo bash setup.sh
---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update system packages
+sudo apt update
+sudo apt upgrade
+
+# Prepare setup to docker installation
+sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+
+# Install docker 
+sudo apt install docker-ce
